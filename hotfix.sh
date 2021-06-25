580aa583b469a96d551a2dbba8d7353d8edd8ee9
Martino Pulici <martinomarelakota@yahoo.it>
2021-06-26 00:08:38 +0200

    Major restructuring
     * bump-version.sh: update
     * dish.csv: move to data/dish.csv
     * food.csv: move to data/food.csv
     * unlimited.txt: move to data/unlimited.txt
     * hotfix.sh: update
     * meal-plan-optimizer.ipynb: update
     * branch_and_bound.py: rename to meal_plan_optimizer.py and update

--- hotfix.sh
+++ hotfix.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sed -i '' 's/__version__ = .*/__version__ = '\'$1\''/' branch_and_bound.py
+sed -i '' 's/__version__ = .*/__version__ = '\'$1\''/' meal_plan_optimizer.py
 sed -i '' 's/__version__ = .*/__version__ = '\'$1\''\\n",/' meal-plan-optimizer.ipynb
+
+doxygen meal_plan_optimizer
+cd latex
+make
+cd ..
+mv latex/refman.pdf docs/refman.pdf
+rm -r html
+rm -r latex
 
 git commit -a -m "Bump version number to "$1""
 git checkout master
