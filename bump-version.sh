0f2fbc0334f3bd1c6af928a081a37375c5256c6d
Martino Pulici <martinomarelakota@yahoo.it>
2021-06-26 10:15:27 +0200

    Minor fix
     * bump-version.sh: minor fix
     * hotfix.sh: minor fix

--- bump-version.sh
+++ bump-version.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 sed -i '' 's/__version__ = .*/__version__ = '\'$1\''/' **/*.py
-sed -i '' 's/__version__ = .*/__version__ = '\'$1\''\\n",/' **/*.ipynb
+sed -i '' 's/__version__ = .*/__version__ = '\'$1\''\\n",/' *.ipynb
 
 pip freeze > requirements.txt
 
 pdoc -o ./docs --docformat numpy meal_plan_optimizer
 
-git commit -a -m "Bump version number to "$1""
+git add .
+git commit -m "Bump version number to "$1""
 git checkout master
 git merge release-$1
 git tag $1
